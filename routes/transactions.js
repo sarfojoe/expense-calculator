ae73f1862dff733f023ccfebb0bea4d6d1c75bf2
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-12-30 15:38:17 +0200

    router updated

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => res.send("Hello"));
+const { getTransactions } = require("../controllers/transactions");
+router.route("/").get(getTransactions);
 
 module.exports = router;

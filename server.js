d6f538e75256ea966e6f03e72e36e21f83432687
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-12-30 14:32:59 +0200

    routes folder added

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 
+dotenv.config({ path: "./config/config.env" });
+
+const transactions = require("./routes/transactions");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 

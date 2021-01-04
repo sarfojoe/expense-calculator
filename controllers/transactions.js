4a360f6ef237b749557777914cc6c38de84924e9
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2021-01-04 17:50:03 +0200

    router updated

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,19 @@
+// @desc    Get all transactions
+// @route   GET /api/v1/transactions
+// @access  Public
+
 exports.getTransactions = (req, res, next) => {
     res.send("GET transactions");
+};
+
+// @desc    Add transactions
+// @route   POST /api/v1/transactions
+// @access  Public
+
+exports.addTransaction = (req, res, next) => {
+    res.send("POST transaction");
+};
+
+exports.deleteTransaction = (req, res, next) => {
+    res.send("DELETE transaction");
 };
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getTransactions } = require("../controllers/transactions");
-router.route("/").get(getTransactions);
+const {
+    getTransactions,
+    addTransaction,
+    deleteTransaction,
+} = require("../controllers/transactions");
+
+router.route("/").get(getTransactions).post(addTransaction);
+
+router.route("/:id").delete(deleteTransaction);
 
 module.exports = router;
